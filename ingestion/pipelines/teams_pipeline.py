1d270b7c991b5772e4b04d49974c59603325a8a8
ebt35 <eyoub.beraki@hotmail.com>
2025-12-22 17:06:45 +0100

    flatten team

--- ingestion/pipelines/teams_pipeline.py
+++ ingestion/pipelines/teams_pipeline.py
@@ -12,3 +12,24 @@ from ingestion.utils.logger import setup_logger
 from ingestion.config import LEAGUE_ID, SEASON, DUCKDB_PATH
 
 logger = setup_logger("teams_pipeline")
+
+def flatten_team(team_data):
+    team = team_data.get("team", {})
+    venue = team_data.get("venue", {})
+    
+    return {
+        "team_id": team.get("id"),
+        "team_name": team.get("name"),
+        "team_code": team.get("code"),
+        "team_country": team.get("country"),
+        "team_founded": team.get("founded"),
+        "team_national": team.get("national"),
+        "team_logo": team.get("logo"),
+        "venue_id": venue.get("id"),
+        "venue_name": venue.get("name"),
+        "venue_address": venue.get("address"),
+        "venue_city": venue.get("city"),
+        "venue_capacity": venue.get("capacity"),
+        "venue_surface": venue.get("surface"),
+        "venue_image": venue.get("image")
+    }
