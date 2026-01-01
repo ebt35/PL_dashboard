18fc0b5823902646775ea3dfd6607224fb2f7dc1
ebt35 <eyoub.beraki@hotmail.com>
2026-01-01 21:13:47 +0100

    updated

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,6 +1,43 @@
+# import os
+# from dagster import Definitions, define_asset_job, AssetSelection
+# from dagster.asset import AssetExecutionContext
+# from .assets import (
+#     teams_data,
+#     fixtures_data,
+#     standings_data,
+#     scorers_data,
+#     players_data,
+#     dbt_transformations,
+#     dbt_resource,
+# )
+
+# pl_mvp_pipeline = define_asset_job(
+#     name="pl_mvp_pipeline",
+#     selection=AssetSelection.all(),
+#     description="Premier League MVP Pipeline: Runs all ingestion pipelines and dbt transformations",
+# )
+
+# defs = Definitions(
+#     assets=[
+#         teams_data,
+#         fixtures_data,
+#         standings_data,
+#         scorers_data,
+#         players_data,
+#         dbt_transformations,
+#     ],
+#     resources={"dbt": dbt_resource},
+#     jobs=[pl_mvp_pipeline],
+# )
+
 import os
-from dagster import Definitions, define_asset_job, AssetSelection
-from dagster.asset import AssetExecutionContext
+from dagster import (
+    Definitions,
+    define_asset_job,
+    AssetSelection,
+    in_process_executor,
+)
+
 from .assets import (
     teams_data,
     fixtures_data,
@@ -28,4 +65,5 @@ defs = Definitions(
     ],
     resources={"dbt": dbt_resource},
     jobs=[pl_mvp_pipeline],
+    executor=in_process_executor,
 )
